7776564e485361f4e6a824541236de73fe6e4f28
Matthew Gibbs <me@matt-gibbs.com>
2018-01-28 23:18:55 +0000

    Initial Commit

---
+++ Wijnderlust/FirstViewController.swift
@@ -0,0 +1,26 @@
+//
+//  FirstViewController.swift
+//  Wijnderlust
+//
+//  Created by Matt Gibbs on 25/01/2018.
+//  Copyright © 2018 MG Creative Services. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let wijnderlustRed = UIColor(red: 149/255.0, green: 0/255.0, blue: 0/255.0, alpha: 1.0)
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
