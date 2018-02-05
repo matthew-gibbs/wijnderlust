c4239ac26a02428d3760a04f5b03fad7ee1df3fa
Matthew Gibbs <me@matt-gibbs.com>
2018-02-05 17:20:53 +0000

    Sets up a ReviewCell object. More work to be done on the data call and structure of reviews

---
+++ Wijnderlust/Profile Views/ReviewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ReviewCell.swift
+//  Wijnderlust
+//
+//  Created by Matt Gibbs on 03/02/2018.
+//  Copyright © 2018 MG Creative Services. All rights reserved.
+//
+
+import UIKit
+
+class ReviewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
